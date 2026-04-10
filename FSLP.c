c6e66e15987405fd7e944a26678ca6820e319530
fr1ght768 <turlybayzhangir@gmail.com>
2026-04-10 19:43:11 +0300

    Initial commit: basic shell functionality

---
+++ FSLP.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <direct.h>
+#include <stdbool.h> 
+#include <dirent.h>
+#include <windows.h>
+
+void clear_screen() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+int main(){
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    char cwd[1024];
+    char *arg;
+    char command[50];
+
+    printf("FSLP\n");
+    printf("Fr1ght's Shell Like Program\n\n");
+
+    while (true){
+        if (_getcwd(cwd, sizeof(cwd)) != NULL){
+            printf("%s> ", cwd);
+        }
+        else{
+            perror("getcwd error");
+            printf("> ");
+        }
+        fgets(command, sizeof(command), stdin);
+        if (strlen(command) > 0){
+            command[strlen(command) - 1] = '\0';
+        }
+
+        char *cmd_name = strtok(command, " ");
+        arg = strtok(NULL, "");
+
+        if (cmd_name == NULL) continue;
+
+
+        if (strcmp(cmd_name, "mkdir") == 0){
+            if(arg == NULL){
+                printf("Error: mkdir requires a folder name\n");
+            }
+            else if(_mkdir(arg) == 0){
+                printf("Directory %s created succesully!\n", arg);
+            }
+            else{
+                perror("Error");
+            }
+        }
+        else if(strcmp(cmd_name, "ls") == 0){
+            struct dirent *de;
+
+            char *target = (arg == NULL) ? "." : arg;
+            DIR *dr = opendir(target);
+
+            if (dr == NULL){
+                perror("Error opening directory");
+            }
+            else{
+                while((de = readdir(dr)) != NULL){
+                    printf("%s\n", de->d_name);
+                }
+                closedir(dr);
+            }
+        }
+        
+        else if(strcmp(cmd_name,"rmdir") == 0){
+            if (arg == NULL){
+                printf("Error: rmdir requires a folder\n");
+            }
+            else if (_rmdir(arg) == 0){
+                printf("Directory %s deleted succesfully\n", arg);
+            }
+            else{
+                perror("Error");
+            }
+        }
+
+        else if(strcmp(cmd_name, "clear") == 0){
+                clear_screen();
+            }
+        
+        else if(strcmp(cmd_name, "cd") == 0){
+            if (arg == NULL){
+                printf("Error: you must write a path\n");
+            }
+            else if(_chdir(arg) == 0){
+                printf("You changed directory to %s succesfully!\n", arg);
+            }
+            else{
+                perror("Error\n");
+            }
+        }
+
+        else if (strcmp(cmd_name, "quit") == 0){
+            break;
+        }
+
+
+        else{
+            printf("The command %s is not defined!\n", command);
+        }
+    }
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+## FSLP (Fr1ght's Shell Like Program)
+simple shell like program for windows written on c
+
+## Features
+- 'ls' - list files in current direcrtory
+- 'mkdir' - create directories in current directory
+- 'rmdir' - delete directories in current directories
+- 'cd' - change current directory
+- 'clear' - clears output
+- 'quit' - to exit the program
+
+## How to compile
+Download MinGW (GCC):
+''' bash
+gcc FSLP.c -o FSLP.exe
+./FSLP.exe
+
+Hope you enjoy!
+
+
